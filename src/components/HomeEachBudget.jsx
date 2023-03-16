606edd15400ec32b7b59916cded51a3d5ff1fe88
WEI TSE <curry900312@gmail.com>
2023-03-16 21:11:02 +0800

    fix HomeEachBudget css

--- src/components/HomeEachBudget.jsx
+++ src/components/HomeEachBudget.jsx
@@ -7,11 +7,11 @@ function HomeEachBudget({ budget }) {
   const navigate = useNavigate()
   return (
     <div className="">
-      <div className="flex justify-between font-Neucha bg-lightblue-ocean text-white p-3 rounded-lg">
-        <div>{budget.date}</div>
-        <div>{budget.name}</div>
-        <div>{budget.category}</div>
-        <div>{currency.format(budget.price)}</div>
+      <div className="flex font-Neucha bg-lightblue-ocean text-white p-3 rounded-lg">
+        <div className="basis-1/4">{budget.date}</div>
+        <div className="basis-1/4">{budget.name}</div>
+        <div className="basis-1/4">{budget.category}</div>
+        <div className="basis-1/4">{currency.format(budget.price)}</div>
         <div onClick={()=>navigate(`/editbudget/${budget.id}`)} className="relative flex justify-center items-center cursor-pointer group">
           <EditIcon sx={{}}/>
           <div className="absolute hidden bg-gray-400 bottom-7 p-1 rounded-xl group-hover:block">
