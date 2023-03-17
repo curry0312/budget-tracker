6886ad67e312312ed9e928e931c7d495be32d4c0
WEI TSE <curry900312@gmail.com>
2023-03-17 13:13:54 +0800

    edit pathname

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { store } from "./store/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename="/budget-tracker">
     <Provider store={store}>
       <App />
     </Provider>
